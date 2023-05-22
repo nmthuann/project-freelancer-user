15a20a15f3eef1b0560bd1a2d213e51fe0cff0e4
Nguyen Minh Thuan <96782417+nmthuann@users.noreply.github.com>
2023-05-22 17:25:00 +0700

    commit_22_5
    - fix something

--- src/modules/profile-document/auth-post.service.ts
+++ src/modules/profile-document/auth-post.service.ts
@@ -2,10 +2,7 @@ import { Consumer, Kafka, logLevel } from 'kafkajs';
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ProducerService } from 'src/modules/kafka/producer.service';
 import { ConsumerService } from 'src/modules/kafka/consumer.service';
-import { EventPattern, MessagePattern } from '@nestjs/microservices';
-import { AccountUserService } from '../account-users/accountUser.service';
 import { ProfileDocumentService } from '../profile-document/profileDocument.service';
-import { AuthService } from '../authentication/auth.service';
 
 @Injectable()
 export class AuthPostService {// implements OnModuleInit
@@ -18,15 +15,13 @@ export class AuthPostService {// implements OnModuleInit
   private readonly consumer: Consumer = this.kafka.consumer({ groupId: 'auth-service' });
 
   constructor(
-    private producerService: ProducerService,
-    private consumerService: ConsumerService,
+    private readonly producerService: ProducerService,
+    private readonly consumerService: ConsumerService,
     private readonly profildocumentService: ProfileDocumentService,
   ){
   }
   
 
-
-  //   login
   async handlePostServiceMessage(groupId: string, topic: string){
     this.consumerService.consume(   
       groupId,
@@ -46,21 +41,4 @@ export class AuthPostService {// implements OnModuleInit
     await this.handlePostServiceMessage('auth-service', 'get-freelancer-name');
   }
 
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
